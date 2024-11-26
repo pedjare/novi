04a6f6d01fcecaff7ae1d8f11bc10d5981d38aa3
pedjare <predrag20nenic@gmail.com>
2024-11-26 12:34:22 +0100

    first commit

---
+++ src/Ime.java
@@ -0,0 +1,11 @@
+public class Ime {
+    private String ime;
+    public Ime(String ime) {
+        this.ime = ime;
+    }
+
+    public void ispis(String ime) {
+        System.out.println(ime);
+    }
+
+}
